db31fe0b303d465928eabdb721375ec78cf5102b
Dustin <dustin.zeigler@fullsail.edu>
2016-08-20 07:27:38 -0400

    Added silent: true to dotenv config...maybe this will fix Code Ships shit...

--- src/models/db.js
+++ src/models/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-require('dotenv').config();
+require('dotenv').config({ silent: true });
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
