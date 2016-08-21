1b959e8b074d9202f39d3d6bac24b486fbc9e839
Dustin <dustin.zeigler@fullsail.edu>
2016-08-21 11:50:05 -0400

    confirming that everything works before making video for assignment 7

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const utilTool = require('utility-tool');
 
-const app = express();
+const app = express();d
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
