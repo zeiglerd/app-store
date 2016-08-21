8266289c5f6e8af00268b5756d1f5b58409e251c
Dustin <dustin.zeigler@fullsail.edu>
2016-08-20 22:36:15 -0400

    Improved model unit tests

--- test/__models_apps.js
+++ test/__models_apps.js
@@ -1,19 +1,19 @@
 const expect = require('chai').expect;
 const faker = require('faker');
 const App = require('../src/models/app');
-// const utilTool = require('utility-tool');
+const utilTool = require('utility-tool');
 
 describe('App Model', () => {
   // Find all apps
   it('GET /api/v1/apps - Find all apps', (done) => {
-    App.all((error) => {
-      throw new Error(error);
+    App.all((err) => {
+      utilTool.debug(err);
     }, (apps) => {
       // Apps (Array) should be a length greater than 0
       expect(apps.length).to.be.above(0);
 
       // Save the returned data for later use in tests
-      this.tstAppIgnored = apps[0].dataValues;
+      this.tstApp = apps[0].dataValues;
 
       done();
     });
@@ -22,13 +22,11 @@ describe('App Model', () => {
   // Find app by id
   it('GET /api/v1/apps/:id - Find app by id', (done) => {
     // Call app model for finding
-    App.one(this.tstAppIgnored, (error) => {
-      throw new Error(error);
-    }, (pApp) => {
-      const app = pApp.dataValues;
-
+    App.one(this.tstApp, (err) => {
+      utilTool.debug(err);
+    }, (app) => {
       // App.title should match fakeApp.title
-      expect(app.title).to.be.equal(this.tstAppIgnored.title);
+      expect(app.dataValues.title).to.be.equal(this.tstApp.title);
 
       done();
     });
@@ -40,16 +38,14 @@ describe('App Model', () => {
     const fakeApp = { title: faker.name.firstName() };
 
     // Call app model for adding
-    App.add(fakeApp, (error) => {
-      throw new Error(error);
-    }, (pApp) => {
-      const app = pApp.dataValues;
-
+    App.add(fakeApp, (err) => {
+      utilTool.debug(err);
+    }, (app) => {
       // App.title should match fakeApp.title
-      expect(app.title).to.be.equal(fakeApp.title);
+      expect(app.dataValues.title).to.be.equal(fakeApp.title);
 
       // Save the returned data for later use in tests
-      this.tstAppIgnored = app;
+      this.tstApp = app.dataValues;
 
       done();
     });
@@ -58,19 +54,17 @@ describe('App Model', () => {
   // Update a App
   it('POST /api/v1/apps/:id - Update a App', (done) => {
     // Update the title of the app
-    this.tstAppIgnored.title = 'Not A Real Name';
+    this.tstApp.title = 'Not A Real Name';
 
     // Call app model for updating
-    App.update(this.tstAppIgnored, (error) => {
-      throw new Error(error);
-    }, (pApp) => {
-      const app = pApp.dataValues;
-
-      // App.title should match this.tstAppIgnored.title
-      expect(app.title).to.be.equal(this.tstAppIgnored.title);
+    App.update(this.tstApp, (err) => {
+      utilTool.debug(err);
+    }, (app) => {
+      // App.title should match this.tstApp.title
+      expect(app.dataValues.title).to.be.equal(this.tstApp.title);
 
       // Save the returned data for later use in tests
-      this.tstAppIgnored = app;
+      this.tstApp = app.dataValues;
 
       done();
     });
@@ -79,14 +73,14 @@ describe('App Model', () => {
   // Delete app by id
   it('DELETE /api/v1/apps/:id - Delete app by id', (done) => {
     // Let Sequelize know to forcefully remove the value, if paranoid.
-    this.tstAppIgnored.force = true;
+    this.tstApp.force = true;
 
     // Call app model for updating
-    App.remove(this.tstAppIgnored, (error) => {
-      throw new Error(error);
-    }, (response) => {
+    App.remove(this.tstApp, (err) => {
+      utilTool.debug(err);
+    }, (data) => {
       // if successfully removed a 1 should be returned
-      expect(response).to.be.equal(1);
+      expect(data).to.be.equal(1);
 
       done();
     });
--- test/__models_users.js
+++ test/__models_users.js
@@ -1,18 +1,19 @@
 const expect = require('chai').expect;
 const faker = require('faker');
 const User = require('../src/models/user');
+const utilTool = require('utility-tool');
 
 describe('User Model', () => {
   // Find all users
   it('GET /api/v1/users - Find all users', (done) => {
-    User.all((error) => {
-      throw new Error(error);
+    User.all((err) => {
+      utilTool.debug(err);
     }, (users) => {
       // Users (Array) should be a length greater than 0
       expect(users.length).to.be.above(0);
 
       // Save the returned data for later use in tests
-      this.tstUserIgnored = users[0].dataValues;
+      this.tstUser = users[0].dataValues;
 
       done();
     });
@@ -21,13 +22,11 @@ describe('User Model', () => {
   // Find user by id
   it('GET /api/v1/users/:id - Find user by id', (done) => {
     // Call user model for finding
-    User.one(this.tstUserIgnored, (error) => {
-      throw new Error(error);
+    User.one(this.tstUser, (err) => {
+      utilTool.debug(err);
     }, (user) => {
-      const tmpUser = user.dataValues;
-
       // User.name should match fakeUser.name
-      expect(tmpUser.name).to.be.equal(this.tstUserIgnored.name);
+      expect(user.dataValues.name).to.be.equal(this.tstUser.name);
 
       done();
     });
@@ -39,8 +38,8 @@ describe('User Model', () => {
     const fakeUser = { name: faker.name.firstName() };
 
     // Call user model for adding
-    User.add(fakeUser, (error) => {
-      throw new Error(error);
+    User.add(fakeUser, (err) => {
+      utilTool.debug(err);
     }, (user) => {
       const tmpUser = user.dataValues;
 
@@ -48,7 +47,7 @@ describe('User Model', () => {
       expect(tmpUser.name).to.be.equal(fakeUser.name);
 
       // Save the returned data for later use in tests
-      this.tstUserIgnored = tmpUser;
+      this.tstUser = tmpUser;
 
       done();
     });
@@ -57,19 +56,17 @@ describe('User Model', () => {
   // Update a User
   it('POST /api/v1/users/:id - Update a User', (done) => {
     // Update the name of the user
-    this.tstUserIgnored.name = 'Not A Real Name';
+    this.tstUser.name = 'Not A Real Name';
 
     // Call user model for updating
-    User.update(this.tstUserIgnored, (error) => {
-      throw new Error(error);
+    User.update(this.tstUser, (err) => {
+      utilTool.debug(err);
     }, (user) => {
-      const tmpUser = user.dataValues;
-
       // User.name should match tempUser.name
-      expect(tmpUser.name).to.be.equal(this.tstUserIgnored.name);
+      expect(user.dataValues.name).to.be.equal(this.tstUser.name);
 
       // Save the returned data for later use in tests
-      this.tstUserIgnored = tmpUser;
+      this.tstUser = user.dataValues;
 
       done();
     });
@@ -78,14 +75,14 @@ describe('User Model', () => {
   // Delete user by id
   it('DELETE /api/v1/users/:id - Delete user by id', (done) => {
     // Let Sequelize know to forcefully remove the value, if paranoid.
-    this.tstUserIgnored.force = true;
+    this.tstUser.force = true;
 
     // Call user model for updating
-    User.remove(this.tstUserIgnored, (error) => {
-      throw new Error(error);
-    }, (response) => {
+    User.remove(this.tstUser, (err) => {
+      utilTool.debug(err);
+    }, (data) => {
       // if successfully removed a 1 should be returned
-      expect(response).to.be.equal(1);
+      expect(data).to.be.equal(1);
 
       done();
     });
