51dcaab46848a749e9079e624c6c854626037333
Dustin <dustin.zeigler@fullsail.edu>
2016-08-21 08:40:52 -0400

    trying SCTIPT_PATH=-zsh, this time wihtout the $...

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const utilTool = require('utility-tool');
 
-const app = express();fffff
+const app = express();ffffff
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
