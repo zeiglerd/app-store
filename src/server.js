8b431f2a38c66df7ed7cecde5d785cc47dd482d6
Dustin <dustin.zeigler@fullsail.edu>
2016-08-21 11:51:19 -0400

    git hook is confirmed working, assignment video is a go

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const utilTool = require('utility-tool');
 
-const app = express();d
+const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
