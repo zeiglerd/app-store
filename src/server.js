76af689443a812a760209261cd0d8014b0f03f60
Dustin <dustin.zeigler@fullsail.edu>
2016-08-21 07:25:17 -0400

    breaking the server for git hook auto restart...again

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const utilTool = require('utility-tool');
 
-const app = express();f
+const app = express();fsd
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
