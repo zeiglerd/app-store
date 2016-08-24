b6d5afe1b4254bfda4ed7b2018935c14de0cba64
Dustin <dustin.zeigler@fullsail.edu>
2016-08-24 18:45:46 -0400

    added jsonfile to package.json and wrote the gulp function to use the vni() function writen by Shaun.

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,52 @@
+const argv = require('yargs').argv;
 const git = require('gulp-git');
 const gulp = require('gulp');
-const argv = require('yargs').argv;
-// const utilTool = require('utility-tool');
+const jsonfile = require('jsonfile');
+const utilTool = require('utility-tool');
 
 gulp.task('gitAdd', () => {
-  console.log(argv.major);
+  console.log();
 
   return gulp.src('./')
     .pipe(git.add());
 });
 
 gulp.task('gitCommit', () => {
+  console.log();
+
   return gulp.src('./')
     .pipe(git.commit('auto commit message...'));
 });
 
-gulp.task('commitAll', ['gitAdd', 'gitCommit']);
+gulp.task('version', () => {
+  jsonfile.readFile('package.json', (err, obj) => {
+    if (err) {
+      utilTool.debug('Failed to read the current version from package.json.');
+    } else if (!argv.major && !argv.minor && !argv.patch) {
+      utilTool.debug('Cannot use this script without --major, --minor or --patch.');
+    } else {
+      let type;
+
+      if (argv.major) {
+        type = 'major';
+      } else if (argv.minor) {
+        type = 'minor';
+      } else if (argv.patch) {
+        type = 'patch';
+      }
+
+      const newObj = obj;
+      newObj.version = utilTool.vni(newObj.version, type);
+
+      jsonfile.writeFile('package.json', newObj, (err) => {
+        if (err) {
+          utilTool.debug('Could not update the version in package.json.');
+        } else {
+          utilTool.debug('Upated the version in package.json.');
+        }
+      });
+    }
+  });
+});
+
+gulp.task('commitAll', ['version', 'gitAdd', 'gitCommit']);
