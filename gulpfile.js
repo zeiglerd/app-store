25a61861290dea82502c80167903792d1bfde537
Dustin <dustin.zeigler@fullsail.edu>
2016-08-24 20:38:39 -0400

    Finished the verion task in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -26,7 +26,9 @@ gulp.task('version', () => {
     if (err) {
       utilTool.debug('Failed to read the current version from package.json.');
     } else if (!argv.major && !argv.minor && !argv.patch) {
-      utilTool.debug('Cannot use this script without --major, --minor or --patch.');
+      utilTool.debug('Cannot use this script without: --major, --minor or --patch');
+    } else if (!argv.m) {
+      utilTool.debug('Cannot use this script without: --m "a meaningful commit message"');
     } else {
       let type;
 
@@ -41,11 +43,11 @@ gulp.task('version', () => {
       const newObj = obj;
       newObj.version = utilTool.vni(newObj.version, type);
 
-      jsonfile.writeFile('package.json', newObj, (err) => {
+      jsonfile.writeFile('package.json', newObj, { spaces: 2 }, (err) => {
         if (err) {
           utilTool.debug('Could not update the version in package.json.');
         } else {
-          utilTool.debug('Upated the version in package.json.');
+          utilTool.debug(`Upated the version to ${newObj.version} in package.json.`, null, 0);
         }
       });
     }
